0f9733496dfc2321b549ee09c26361ebf942f145
local_snishiwaki <local_snishiwaki@PC1309D.local>
2019-08-06 16:22:27 +0900

    Correct

--- db/db.php
+++ db/db.php
@@ -1,6 +1,6 @@
 <?php
 try {
-  $sql = new PDO ('mysql:dbname=mf; charset=utf8', 'root', 'yoro12');
+  $pdo = new PDO ('mysql:dbname=mf; charset=utf8', 'root', 'yoro12');
   print '接続に成功しました。';
 } catch (PDOException $e) {
   print "接続エラー:{$e->getMessage()}";
