0984c23d72f681bd0ad4d540afdf837fde59e197
local_snishiwaki <local_snishiwaki@PC1309D.local>
2019-08-07 10:24:18 +0900

    add php post

---
+++ post/sample-post1.php
@@ -0,0 +1,15 @@
+https://www.sejuku.net/blog/27843
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>POST_SAMPLE</title>
+</head>
+<body>
+<form method="POST" action="sample-post2.php">
+ <label>あなたの好きなものは？：</label><br />
+ <input type="text" name="data" /><br />
+ <input type="submit" value="送信" />
+</form>
+</body>
+</html>
---
+++ post/sample-post2.php
@@ -0,0 +1,14 @@
+<!-- see:https://www.sejuku.net/blog/27843 -->
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>POST_SAMPLE</title>
+</head>
+<body>
+<?php
+ echo "あなたの好きなものは「".$_POST["data"] ."」です。";
+?>
+</form>
+</body>
+</html>
