be5562d8eba6e9fd4654a55e88a2c5c398fb10f7
local_snishiwaki <local_snishiwaki@PC1309D.local>
2019-08-07 17:12:20 +0900

    insert db

--- post/sample-post2.php
+++ post/sample-post2.php
@@ -6,9 +6,35 @@
 <title>POST_SAMPLE</title>
 </head>
 <body>
+
 <?php
  echo "あなたの好きなものは「".$_POST["data"] ."」です。";
 ?>
+
+<?php
+require_once dirname(__FILE__) . '/../../DSN.php';
+
+try {
+  $pdo = new PDO("mysql:dbname=$dsn[dbname]; charset=utf8", $dsn['user'], $dsn['pass']);
+  print '接続に成功しました。';
+} catch(PDOException $e) {
+  print "接続エラー:{$e->getMessage()}";
+}
+?>
+
+<?php
+  // INSERT文を変数に格納
+  $sql = "insert into things (name) values (:name)";
+
+  // 挿入する値は空のまま、SQL実行の準備をする
+  $stmt = $pdo->prepare($sql);
+  // 挿入する値を配列に格納する
+  $params = array(':name' => $_POST["data"]);
+
+  // 挿入する値が入った変数をexecuteにセットしてSQLを実行
+  $stmt->execute($params);
+?>
+
 </form>
 </body>
 </html>
