24d815338ad1ba68daad1b6956b76c949e8e3082
local_snishiwaki <local_snishiwaki@PC1309D.local>
2019-08-16 11:10:21 +0900

    update session code

---
+++ session/page2.php
@@ -0,0 +1,15 @@
+<?php
+// see : http://phpspot.net/php/man/php/function.session-start.html
+// page2.php
+
+session_start();
+
+echo 'Welcome to page #2<br />';
+
+echo $_SESSION['favcolor']; // green
+echo $_SESSION['animal'];   // cat
+echo date('Y m d H:i:s', $_SESSION['time']);
+
+// page1.php と同様に、ここで SID を使うこともできます
+echo '<br /><a href="page1.php">page 1</a>';
+?>
--- session/start.php
+++ session/start.php
@@ -1,5 +1,4 @@
 <?php
 session_start();
 $_SESSION['username'] = "nishiwaki";
-echo $_SESSION['username'];
 ?>
